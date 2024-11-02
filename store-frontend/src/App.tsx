7557b8754caa4c77adea7e6df2b28836d0a73b49
Pedro Vieira <pedrovieirafdev@gmail.com>
2024-11-02 14:29:52 -0300

    initial commit

--- store-frontend/src/App.tsx
+++ store-frontend/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App() { // initial commit
   const [count, setCount] = useState(0)
 
   return (
