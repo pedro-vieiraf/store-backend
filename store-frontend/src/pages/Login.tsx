cfb91dba82455366327c301f10a58bd4cb667af0
Pedro Vieira <pedrovieirafdev@gmail.com>
2024-11-28 16:57:36 -0300

    🖌️ style: Login Page

--- store-frontend/src/pages/Login.tsx
+++ store-frontend/src/pages/Login.tsx
@@ -43,15 +43,25 @@ function Login() {
     // colocar uma mensagem de login inválido caso esteja errado
 
     return (
-        <>
-            <h1>Welcome!</h1>
-            <form onSubmit={ handleSubmit }>
-                <input type="email" onChange={ handleChange } name="email" id="email" placeholder="Enter your email address"/>
-                <input type="password" onChange={ handleChange } name="password" id="password" placeholder="Enter your password"/>
-                <button>Sign In</button>
-                <p>New here? <Link to="/register">Sign up</Link></p>
-            </form>
-        </>
+        <div className="w-full rounded-lg md:mt-0 sm:max-w-md xl:p-0 bg-eeire-black-200">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8 text-left"> 
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                    Sign in to your account
+                </h1>
+                <form className="space-y-4 md:space-y-6" onSubmit={ handleSubmit }>
+                    <div>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                        <input type="email" onChange={ handleChange } name="email" id="email" placeholder="Enter your email address" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"/>
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                        <input type="password" onChange={ handleChange } name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"/>
+                    </div>
+                    <button className="w-full text-white bg-saffron-200 hover:bg-saffron-400">Sign In</button>
+                    <p className="text-sm font-light text-gray-500">Don't have an account yet? <Link to="/register">Sign up</Link></p>
+                </form>
+            </div>
+        </div>
     )
 }
 
