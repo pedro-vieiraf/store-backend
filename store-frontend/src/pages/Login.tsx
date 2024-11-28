462c461c2a5790e0e3cd30b4dd66ba00ffd8e754
Pedro Vieira <pedrovieirafdev@gmail.com>
2024-11-28 17:16:57 -0300

    🖌️ style: Register Page

--- store-frontend/src/pages/Login.tsx
+++ store-frontend/src/pages/Login.tsx
@@ -45,20 +45,20 @@ function Login() {
     return (
         <div className="w-full rounded-lg md:mt-0 sm:max-w-md xl:p-0 bg-eeire-black-200">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8 text-left"> 
-                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl">
                     Sign in to your account
                 </h1>
                 <form className="space-y-4 md:space-y-6" onSubmit={ handleSubmit }>
                     <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                        <input type="email" onChange={ handleChange } name="email" id="email" placeholder="Enter your email address" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"/>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium">Your email</label>
+                        <input type="email" onChange={ handleChange } name="email" id="email" placeholder="Enter your email address" className="border rounded-lg block w-full p-2.5"/>
                     </div>
                     <div>
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                        <input type="password" onChange={ handleChange } name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"/>
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium">Password</label>
+                        <input type="password" onChange={ handleChange } name="password" id="password" placeholder="••••••••" className="border rounded-lg block w-full p-2.5"/>
                     </div>
                     <button className="w-full text-white bg-saffron-200 hover:bg-saffron-400">Sign In</button>
-                    <p className="text-sm font-light text-gray-500">Don't have an account yet? <Link to="/register">Sign up</Link></p>
+                    <p className="text-sm font-light">Don't have an account yet? <Link to="/register">Sign up</Link></p>
                 </form>
             </div>
         </div>
